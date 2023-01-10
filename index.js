d0be44297166c3c9f82ad2eed8102f7f34efe033
Nadeem740 <nadeemblayparambil@gmail.com>
2023-01-10 11:46:49 +0530

    cors-added

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const query=require('./q.js')
 const query1=require('./a.js')
 const query2=require('./b.js')
 const query3=require('./c.js')
+const cors=require("cors");
 const app=express();
-const port=3000
+const port=8000
 app.use(bodyParser.json())
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.get('/bookinghistory',query2.bookinghistory);
 app.get('/bookroom',query1.bookingroom);
